918f941ccc1028aeaee80962d65aae85151bed16
Ido Feins <ifeins@gmail.com>
2013-06-15 19:48:57 +0300

    added the display of category if it's listed.

--- app/serializers/restaurant_serializer.rb
+++ app/serializers/restaurant_serializer.rb
@@ -1,3 +1,7 @@
 class RestaurantSerializer < ActiveModel::Serializer
-  attributes :id, :name, :logo_url
+  attributes :id, :name, :logo_url, :category
+
+  def category
+    object.category.try(:name)
+  end
 end
