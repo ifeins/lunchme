d0ce027db612b219c4a9c01f3f5b273554be8f66
Ido Feins <ifeins@gmail.com>
2013-06-26 16:21:54 +0300

    removed categories table as it is unused.

--- db/schema.rb
+++ db/schema.rb
@@ -37,12 +37,6 @@ ActiveRecord::Schema.define(:version => 20130601145844) do
     t.datetime "updated_at", :null => false
   end
 
-  create_table "categories", :force => true do |t|
-    t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
   create_table "locations", :force => true do |t|
     t.float    "latitude"
     t.float    "longitude"
