93f45d38a28bf267b14c1f3106a28aebe0a3992b
Ido Feins <ifeins@gmail.com>
2013-07-06 12:57:54 +0300

    normalized provider and fix user account creation.

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -3,14 +3,16 @@ class SessionController < ApplicationController
   respond_to :json, :html
 
   def create
-    account = Account.find_or_create_by_provider_and_uid(auth_hash[:provider], auth_hash[:uid])
-    unless account.user.present?
-      account.create_user(:first_name => auth_hash[:info][:first_name],
-                          :last_name => auth_hash[:info][:last_name],
-                          :email => auth_hash[:info][:email],
-                          :avatar_url => auth_hash[:info][:image]
-      )
-    end
+    account = Account.find_or_create_by_provider_and_uid(
+        normalize_provider(auth_hash[:provider]),
+        auth_hash[:uid],
+        :user_attributes => {
+            :first_name => auth_hash[:info][:first_name],
+            :last_name => auth_hash[:info][:last_name],
+            :email => auth_hash[:info][:email],
+            :avatar_url => auth_hash[:info][:image]
+        }
+    )
 
     respond_with account.user do |format|
       format.html { redirect_to root_url }
@@ -25,6 +27,13 @@ class SessionController < ApplicationController
 
   def auth_hash
     request.env['omniauth.auth']
+  end
+
+  def normalize_provider(provider)
+    case provider.to_sym
+      when :google_oauth2 then :google
+      else :unknown
+    end
   end
 
 end
--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,5 @@
 class Account < ActiveRecord::Base
   belongs_to :user
-  attr_accessible :provider, :uid
+  attr_accessible :provider, :uid, :user_attributes
+  accepts_nested_attributes_for :user
 end
