29d2a09f9115ef59348f6c9c9a0be7facdf34c3f
Ido Feins <ifeins@gmail.com>
2013-08-17 17:14:55 +0300

    initial modal layout and styling.

--- app/serializers/restaurant_serializer.rb
+++ app/serializers/restaurant_serializer.rb
@@ -1,8 +1,12 @@
 class RestaurantSerializer < ActiveModel::Serializer
-  attributes :id, :name, :logo_url
+  attributes :id, :name, :logo_url, :address
   has_many :tags
 
   def logo_url
     object.logo.url
+  end
+
+  def address
+    "#{object.location.street}, #{object.location.city}"
   end
 end
