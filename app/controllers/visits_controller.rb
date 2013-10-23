8b9758a312c05d5aa827ceb0359c8263bca28d1b
Ido Feins <ifeins@gmail.com>
2013-10-23 17:14:30 +0300

    If the user previously selected a restaurant he visited during lunch then attempting to select a new one will simply override the previous entry.

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -6,7 +6,13 @@ class VisitsController < ApplicationController
 
   def create
     restaurant = Restaurant.find(params[:restaurant_id])
-    visit = current_lunch.visits.create(:user => current_user, :restaurant => restaurant)
+    visit = current_lunch.visits.for_user(current_user).first
+    if visit.present?
+      visit.update_attributes(:restaurant => restaurant)
+    else
+      visit = current_lunch.visits.create(:user => current_user, :restaurant => restaurant)
+    end
+
     respond_with visit, :location => lunch_url(current_lunch)
   end
 
--- app/models/visit.rb
+++ app/models/visit.rb
@@ -3,5 +3,7 @@ class Visit < ActiveRecord::Base
   belongs_to :user
   belongs_to :restaurant
 
+  scope :for_user, ->(user) { where(user_id: user.id) }
+
   attr_accessible :user, :restaurant
 end
