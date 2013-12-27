dd432edceaf7ca25288199475bc99ff601bd672f
Ido Feins <ifeins@gmail.com>
2013-12-27 22:09:10 +0200

    fixed join table.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,7 +1,7 @@
 class Tag < ActiveRecord::Base
   belongs_to :tag_definition
   belongs_to :restaurant
-  has_and_belongs_to_many :users
+  has_and_belongs_to_many :users, :join_table => :users_tags
 
   attr_accessible :quantity, :tag_definition
 end
