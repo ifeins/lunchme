d64e51f56662dd26ed96b16f6ebbb4743d73088f
Asaf Gartner <agartner@ebay.com>
2014-04-02 16:54:37 +0300

    Allow cross origin requests
    Allows us to make api requests from other domains

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
 
   before_filter :must_not_be_banned
   after_filter :set_ng_xsrf_token
+  after_filter :allow_cross_origin
 
   def set_ng_xsrf_token
     cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
@@ -15,6 +16,14 @@ class ApplicationController < ActionController::Base
 
   def verified_request?
     super || request.headers['X-XSRF-TOKEN'] == form_authenticity_token
+  end
+
+  def allow_cross_origin
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    headers['Access-Control-Max-Age'] = '1728000'
   end
 
 end
