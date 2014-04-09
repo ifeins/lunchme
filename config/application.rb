4911727aca2b689bd901ec8add10af7f0b627920
Ido Feins <ifeins@gmail.com>
2014-04-09 18:51:49 +0300

    moved comment to right place.

--- config/application.rb
+++ config/application.rb
@@ -1,14 +1,13 @@
 require File.expand_path('../boot', __FILE__)
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 require 'rails/all'
 
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Lunchtime
   class Application < Rails::Application
-    # Configure sensitive parameters which will be filtered from the log file.
     config.active_record.observers = :vote_observer
     config.autoload_paths += Dir["#{config.root}/app/controllers/supports"]
   end
